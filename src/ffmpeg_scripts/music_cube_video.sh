4a8ae137f4b7330918c638e4ad303342eb41e005
Alexander J Turner <nerdscentral@googlemail.com>
2021-02-28 19:39:47 +0000

    Re-use the old vioce-square code to make a mad music visualizerA

--- src/ffmpeg_scripts/music_cube_video.sh
+++ src/ffmpeg_scripts/music_cube_video.sh
@@ -1,19 +1,44 @@
 #!/bin/zsh
 # Description:
-# Make a rotating visualization of audio in a 1080 square for embedding.
+# Make a rotating cube like visualization of audio in a 1920x1080 square for youtube.
 #
 # Args:
 # <video or audio in> <overlay png>
 #
 # Out:
-# <*-square>..nut
+# <*-music-cube>.mp4
 #
 
-. $(dirname "$0")/encoding.sh
+
+# Notes on encoding:
+# ==================
+# - Youtube uses yuv420p so no point trying for something better here.
+# - We could use 4K upscaling to make a nicer video - but the point is music.
+# - Youtube uses 'tv' color range so we output to that to avoid Youtube converting it.
+# - Youtube will crush the quality anyhow so -crf 18 is probaby OK...
+# - Audio is encoding into 256k aac for the final product (which should be good enough).
+#
+# Notes on intermediate filles:
+# =============================
+# - For simplicity this makes tempv.mp4 and tempa.wav (32bit integer 96k) then moshes them
+#   together to form the final file and removes the intermediate.
+# - The main video script is written to run.sh and then executed; this really helps with debugging.:w
+#
+# Notes on performance:
+# =====================
+# - My MBP encodes this at a leisurely 10 fps and gets very hot doing it.
+# - One trick is to add -threads 1 to the command line and just leave it running over night.
+# - Alternatively - work on streamlining the filters.
+
+# Frame rate = 25 is OK 50 is better if you are patient enough.
+r=50
 
 # Bigger z_rate slows down the 'cube' rotation.
 z_rate=$((${r} * 3))
+
 len=$($(dirname "$0")/get_length.sh "${1}")
+exe="$(dirname "$0")/ffmpeg"
+enc="-c:v libx264 -preset medium -tune film -crf 18 -s 1920x1080 -sar 1:1 -r 50 -pix_fmt yuv420p -sws_flags +accurate_rnd+full_chroma_int -colorspace 1 -color_primaries 1 -color_trc 1 -dst_range 1 -color_range 1"
 cmd="${exe} -y -i '${1}' -i '${2}' ${enc} -ss 0 -to ${len} -filter_complex \
 \"
 [1:v]
@@ -100,7 +125,9 @@ drawbox=
     color=white:
     replace=1:
     x=0:
-    y=0
+    y=0,
+lagfun=
+   decay=0.98
 [box];
 
 [box][img]
@@ -215,21 +242,60 @@ overlay
 [ov2][ov1]
 overlay,
 drawbox=
-    width=1078:
-    height=1078:
-    thickness=1:
-    color=0x00FF0088:
-    replace=1:
-    x=1:
-    y=1,
-drawbox=
     width=1080:
     height=1080:
-    thickness=1:
-    color=0x00880000:
+    thickness=2:
+    color=0xFF88FFFF:
     replace=1:
     x=0:
+    y=0,
+lagfun=
+    decay=0.9,
+split=2
+[cube][wformin];
+
+color=
+    c=black:
+    s=1920x1080:
+    r=${r}
+[bg];
+
+[bg][cube]
+overlay=
+    x=420:
     y=0
+[cube_final];
+
+[wformin]
+waveform=
+    m=0:
+    d=0:
+    r=0:
+    c=7,
+crop=
+   x=32:
+   y=0:
+   w=in_w-64:
+   h=in_h,
+scale=
+   size=420x1080:
+   flags=bilinear,
+lagfun=0.99,
+split=2
+[wfl][wfr];
+
+[wfl]
+hflip
+[wflf];
+
+[cube_final][wflf]
+overlay=
+    x=0
+[ov1];
+
+[ov1][wfr]
+overlay=
+    x=1920-420
 [out];
 
 anullsrc=
@@ -247,7 +313,7 @@ echo $cmd > run.sh
 
 $(dirname "$0")/ffmpeg -i "${1}" -c:a pcm_s32le -ar 96K tempa.wav
 
-export cmd="${exe} -i tempa.wav -i tempv.nut -c:v copy -c:a aac -b:a 256k -map 1:v -map 0:a '${1%.*}-square.mp4'"
+export cmd="${exe} -y -i tempa.wav -i tempv.nut -c:v copy -c:a aac -b:a 256k -map 1:v -map 0:a '${1%.*}-music-cube.mp4'"
 echo
 echo '================================================================================'
 echo Will Run ${cmd}
