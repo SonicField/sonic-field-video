c47c94e058796336609a43d0083d6ec44662bd14
Alexander J Turner <nerdscentral@googlemail.com>
2021-03-24 17:50:55 +0000

    Rename smooth to finalize for finishing off HDR 4k content

--- src/ffmpeg_scripts/finalize.sh
+++ src/ffmpeg_scripts/finalize.sh
@@ -1,17 +1,25 @@
 #!/bin/zsh
 # Description:
-# Testing ways to interpolate from 8 bit
+# Use scaling to perform a very slight anti-alias effect.
+# It is very close to a blur but works nicely to not make the image look
+# blurred.
+# I added in a slight gamma twise to improve definition at the high end in
+# HDR and HDR -> SDR downscale for youtube.
 #
 # Args:
 # <video in name>
 #
 # Out:
-# <in name>-smooth.nut
+# <in name>-finalized.nut
 #
 
 . $(dirname "$0")/encoding.sh
-cmd="${exe} -i '${1}' ${enc} -filter_complex \"
+cmd="${exe} -v verbose -i '${1}' ${enc} -filter_complex \"
 [0:v]
+zscale,
+format=gbrp16le,
+curves=
+    all='0/0 0.25/0.25 0.75/0.7 1/1',
 zscale,
 format=gbrpf32le,
 zscale=
@@ -30,7 +38,7 @@ echo
 echo $cmd > run.sh
 . ./run.sh
 
-cmd="${exe} -i tempv.nut -i '$1' -i '$4' -c:v copy -c:a copy -map 0:v -map 1:a '${1%.*}-smooth.nut'"
+cmd="${exe} -i tempv.nut -i '$1' -i '$4' -c:v copy -c:a copy -map 0:v -map 1:a '${1%.*}-finalized.nut'"
 echo
 echo '================================================================================'
 echo Will Run ${cmd}
